97428ef291193441a17eb6051d3e7caebd11a755
Treevor <treevormilan@gmail.com>
2024-03-07 19:20:53 -0800

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,17 @@
+// pages/protect.js
 import { Protect } from "@clerk/nextjs";
  
 export default function ProtectPage() {
   return (
     <Protect
       role="org:admin"
-      fallback={<p>Only a admin can access this content.</p>}
+      fallback={<p>Only an admin can access this content.</p>}
     >
+      {/* Your protected content goes here */}
+      <div>
+        <h1>Protected Content</h1>
+        <p>This content is only accessible to users with the 'org:admin' role.</p>
+      </div>
     </Protect>
   );
 }
