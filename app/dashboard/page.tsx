2b57db69601f09b7d7ef9f9df36adc53a40a6764
Treevor <treevormilan@gmail.com>
2024-04-30 13:17:29 -0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,9 @@
-// ProtectedPage.tsx
 'use client'
-import React, { useEffect, useState } from 'react';
-import { auth } from '../firebase/config';
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button, Accordion, AccordionItem } from "@nextui-org/react";
+import React, { useState, useEffect } from "react";
+import { Dropdown, DropdownTrigger, DropdownMenu, Textarea, DropdownItem, Accordion, AccordionItem, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
 import ServerForm from '../components/ServerForm';
+import { auth } from '../firebase/config';
+
 const allowedUids = ['AohsbxeW2pWIsAfTiCQAtJKga3k2', '4JNPKmw9cjQDKVbMjP5ceJpmxO82'];
 
 const ProtectedPage = () => {
@@ -11,6 +11,10 @@ const ProtectedPage = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState("");
     const [rows, setRows] = useState([]);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [modalSize, setModalSize] = useState("2xl");
+    // Add state to hold the item being edited
+    const [editingItem, setEditingItem] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -52,10 +56,6 @@ const ProtectedPage = () => {
             unsubscribe();
         };
     }, []);
-
-    if (!user) {
-        return <div>Loading...</div>;
-    }
 
     const handleDelete = async (id) => {
         // Show confirmation dialog
@@ -112,6 +112,38 @@ const ProtectedPage = () => {
         }
     };
 
+    const handleSaveChanges = async () => {
+        try {
+            const response = await fetch(`../api/edit/${editingItem._id}`, {
+                method: 'POST',
+                headers: {
+                    Authorization: 'BozRgu8UEY', // Replace with your actual authorization token
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(editingItem) // Send the edited JSON data
+            });
+            if (response.ok) {
+                const updatedResponse = await fetch("../api/realms", {
+                    headers: {
+                        Authorization: "q5VLqNQBZu"
+                    }
+                });
+                const updatedData = await updatedResponse.json();
+                setRows(updatedData);
+            } else {
+                throw new Error("Failed to save changes");
+            }
+        } catch (error) {
+            console.error("Error saving changes:", error);
+            // Handle error
+        }
+    };
+    
+    const handleTextareaChange = (e) => {
+        // Update the editingItem state with the modified JSON content
+        setEditingItem(JSON.parse(e.target.value));
+    };
+
     const exportServerData = (serverData) => {
         const jsonServerData = JSON.stringify(serverData);
         const blob = new Blob([jsonServerData], { type: 'application/json' });
@@ -155,10 +187,17 @@ const ProtectedPage = () => {
         });
     };
 
+    // Function to open modal
+    const openModal = (item) => {
+        setModalSize("2xl"); // Set modal size to 2xl
+        setEditingItem(item); // Set the item being edited
+        onOpen();
+    };
+
     return (
         <main className="montserrat">
             <div className="w-full justify-center items-center flex-col">
-                <h1 className='pb-3'>Welcome, {user.uid}!</h1>
+                <h1 className='pb-3'>Welcome</h1>
 
                 <Accordion>
                     <AccordionItem key="1" aria-label="Accordion 1" title="Server List (Expand)">
@@ -194,7 +233,7 @@ const ProtectedPage = () => {
                                                         <Button
                                                             variant="bordered"
                                                             isIconOnly
-                                                            className='ml-2 '
+                                                            className='ml-2'
                                                         >
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                                                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
@@ -207,7 +246,9 @@ const ProtectedPage = () => {
                                                             if (key === 'delete') {
                                                                 handleDelete(item._id);
                                                             } else if (key === 'export') {
-                                                                exportServerData(item); // Call export function with item data
+                                                                exportServerData(item);
+                                                            } else if (key === 'edit') {
+                                                                openModal(item); // Pass the item being edited to openModal
                                                             }
                                                         }}
                                                     >
@@ -223,14 +264,34 @@ const ProtectedPage = () => {
                             </table>
                         )}
                         {/* End of Database Table */}
-
                     </AccordionItem>
                     <AccordionItem key="2" aria-label="Accordion 1" title="Add Server (Expand)">
                         <ServerForm onSubmit={handleInsert} />
                     </AccordionItem>
-
                 </Accordion>
             </div>
+
+            {/* Modal for editing */}
+            <Modal isOpen={isOpen} onClose={onClose} size={modalSize}>
+                <ModalContent>
+                    <ModalHeader>Edit Item</ModalHeader>
+                    <ModalBody>
+    {editingItem && ( // Check if editingItem is not null
+        <Textarea
+            label="Edit JSON"
+            placeholder="Start Editing the JSON"
+            className="max-w-full"
+            value={JSON.stringify(editingItem, null, 2)} // JSONify the item being edited
+            onChange={handleTextareaChange} // Add onChange handler
+        />
+    )}
+</ModalBody>
+                    <ModalFooter>
+                    <Button color="primary" onClick={() => { handleSaveChanges(); onClose(); }}>Save Changes</Button>
+    <Button color="secondary" onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </main>
     );
 };
